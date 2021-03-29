c825da984ef6da469c3c846c5bccb973c3fd3da7
zonghuang <1683766027@qq.com>
2021-03-29 09:57:13 +0800

    React中使用axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,190 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from "react";
 
-export default class App extends Component {
+import axios from "axios";
+
+import request from "./service/request";
+
+export default class App extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+    };
+  }
+
+  async componentDidMount() {
+    // 1.真实开发逻辑：拿到数据
+    // this.setState({
+    //   products: [...this.state.products, ...res]
+    // })
+
+    // 2.axios发送基本的网络请求
+    // axios({
+    //   url: "https://httpbin.org/get",
+    //   params: {
+    //     id: 1,
+    //     name: "zonghuang"
+    //   }
+    // }).then(res => {
+    //   console.log(res);
+    // }).catch(err => {
+    //   console.error(err);
+    // })
+
+    // axios({
+    //   url: "https://httpbin.org/post",
+    //   params: {
+    //     id: 1,
+    //     name: "zonghuang"
+    //   },
+    //   method: "post"
+    // }).then(res => {
+    //   console.log(res);
+    // }).catch(err => {
+    //   console.error(err);
+    // })
+
+    // 3.axios发送get/post
+    // axios.get("https://httpbin.org/get", {
+    //   params: {
+    //     name: "lilei",
+    //     age: 30
+    //   }
+    // }).then(console.log);
+
+    // axios.post("https://httpbin.org/post", {
+    //   name: "lucy",
+    //   age: 28
+    // }).then(console.log);
+
+    // axios.request({
+
+    // })
+
+    // 4.axios部分源码的阅读
+
+    // 5.await async
+    // try {
+    //   const result = await axios.get("https://httpbin.org/get", {
+    //     params: {
+    //       id: 1,
+    //       name: "zonghuang"
+    //     }
+    //   })
+    //   console.log(result);
+    // } catch(err) {
+    //   console.log(err);
+    // }
+
+    // 6.axios.all
+    // const request1 = axios({
+    //   url: "https://httpbin.org/get",
+    //   params: {id: 1, name: "zonghaung"}
+    // })
+
+    // const request2 = axios({
+    //   url: "https://httpbin.org/post",
+    //   params: {id: 2, name: "hy"},
+    //   method: "post"
+    // })
+
+    // axios.all([request1, request2]).then(([res1, res2]) => {
+    //   console.log(res1, res2);
+    // })
+
+    // const promise1 = new Promise((resolve, reject) => {
+    //   setTimeout(() => {
+    //     resolve("promise1")
+    //   }, 1000);
+    // });
+
+    // const promise2 = new Promise((resolve, reject) => {
+    //   setTimeout(() => {
+    //     resolve("promise2")
+    //   }, 3000);
+    // });
+
+    // axios.all([promise1, promise2]).then(res => {
+    //   console.log(res);
+    // })
+
+    // // 8.创建新的实例
+    // const instance1 = axios.create({
+    //   baseURL: "http://coderwhy.xyz",
+    //   timeout: 5000,
+    //   headers: {
+
+    //   }
+    // })
+
+    // const instance2 = axios.create({
+    //   baseURL: "http://baidu.xyz",
+    //   timeout: 10000,
+    //   headers: {
+
+    //   }
+    // })
+
+    // 9.请求拦截
+    axios.interceptors.request.use(
+      (config) => {
+        // 1.发送网络请求时, 在界面的中间位置显示Loading的组件
+
+        // 2.某一些请求要求用户必须携带token, 如果没有携带, 那么直接跳转到登录页面
+
+        // 3.params/data序列化的操作
+        console.log("请求被拦截");
+
+        return config;
+      },
+      (err) => {}
+    );
+
+    axios.interceptors.response.use(
+      (res) => {
+        return res.data;
+      },
+      (err) => {
+        if (err && err.response) {
+          switch (err.response.status) {
+            case 400:
+              console.log("请求错误");
+              break;
+            case 401:
+              console.log("未授权访问");
+              break;
+            default:
+              console.log("其他错误信息");
+          }
+        }
+        return err;
+      }
+    );
+
+    axios({
+      url: "https://httpbin.org/get",
+      params: {
+        id: 1,
+        name: "zonghuang",
+      },
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+
+    request({
+      url: "/get",
+      params: {
+        name: "why",
+        age: 18,
+      },
+    }).then(console.log);
+  }
+
   render() {
-    return (
-      <div>
-        App
-      </div>
-    )
+    return <div>APP</div>;
   }
 }
-
-
